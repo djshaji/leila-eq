568d7a0032dbcbbbd7f3bd3706cc492b8e9e82f2
Shaji Khan <git@shaji.in>
2021-07-05 02:49:10 +0530

    UI about done

--- app/src/main/java/com/shajikhan/leilaequalizer/ui/home/HomeFragment.java
+++ app/src/main/java/com/shajikhan/leilaequalizer/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.shajikhan.leilaequalizer.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,10 +19,15 @@ import androidx.lifecycle.ViewModelProvider;
 import com.shajikhan.leilaequalizer.R;
 import com.shajikhan.leilaequalizer.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private LinearLayout mLinearLayout;
+    Context context ;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,14 +36,69 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        context = getActivity();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        mLinearLayout = root.findViewById (R.id.eq_ll);
+//        mLinearLayout = new LinearLayout(context);
+
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        short bands = 5 ; //TODO: do this automagically
+        int band_freq [] = {60, 500, 1000, 8000, 14000};
+        for (short i = 0; i < bands; i++) {
+            final short band = i;
+
+            TextView freqTextView = new TextView(context);
+            freqTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            freqTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            freqTextView.setText(band_freq [i] + " Hz");
+            mLinearLayout.addView(freqTextView);
+
+            LinearLayout row = new LinearLayout(context);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView minDbTextView = new TextView(context);
+            minDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            minDbTextView.setText("0 dB");
+
+            TextView maxDbTextView = new TextView(context);
+            maxDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            maxDbTextView.setText("12 dB");
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            layoutParams.weight = 1;
+            SeekBar bar = new SeekBar(context);
+            bar.setLayoutParams(layoutParams);
+            bar.setMax(12);
+            bar.setProgress(6);
+
+            /*
+            bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    mEqualizer.setBandLevel(band, (short) (progress + minEQLevel));
+                }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+             */
+
+            row.addView(minDbTextView);
+            row.addView(bar);
+            row.addView(maxDbTextView);
+
+            mLinearLayout.addView(row);
+        }
+
         return root;
     }
 
